47376f619fcecded8981dc1122f0c08996d7749f
wangchunping2011 <wangchunping1@wangchunpingdemac-mini.local>
2016-01-11 09:12:24 +0800

    Initial Commit

---
+++ SegmentPagesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentPagesDemo
+//
+//  Created by 王春平 on 16/1/11.
+//  Copyright © 2016年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
